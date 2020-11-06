a3b3563cc084c978cabd9b1e8f8a3b80474b410a
georgegarrington <47332406+georgegarrington@users.noreply.github.com>
2020-11-06 15:14:30 +0000

    Finished chapter 8 :))

--- chapter8FindingFaces/src/main/java/uk/ac/soton/ecs/gg2g17/App.java
+++ chapter8FindingFaces/src/main/java/uk/ac/soton/ecs/gg2g17/App.java
@@ -16,7 +16,9 @@ import org.openimaj.image.processing.face.detection.keypoints.FacialKeypoint;
 import org.openimaj.image.processing.face.detection.keypoints.KEDetectedFace;
 import org.openimaj.image.processing.face.util.KEDetectedFaceRenderer;
 import org.openimaj.image.typography.hershey.HersheyFont;
+import org.openimaj.math.geometry.point.Point2d;
 import org.openimaj.math.geometry.point.Point2dImpl;
+import org.openimaj.math.geometry.shape.Ellipse;
 import org.openimaj.math.geometry.shape.Rectangle;
 import org.openimaj.video.VideoDisplay;
 import org.openimaj.video.VideoDisplayListener;
@@ -85,6 +87,7 @@ public class App {
                     frame.drawShape(face.getBounds(), RGBColour.RED);
                     FacialKeypoint[] kps = face.getKeypoints();
 
+
                     for(FacialKeypoint kp : kps){
 
                         Point2dImpl pos = kp.position.clone();
@@ -92,6 +95,31 @@ public class App {
                         frame.drawPoint(pos, RGBColour.GREEN, 3);
 
                     }
+
+                    //cornerPos contains the x and y coordinates of where the corner of the mouth is
+                    FacialKeypoint corner = FacialKeypoint.getKeypoint(kps, FacialKeypoint.FacialKeypointType.MOUTH_LEFT);
+                    Point2dImpl cornerPos = corner.position.clone();
+                    cornerPos.translate(face.getBounds().x, face.getBounds().y);
+                    //frame.drawPoint(cornerPos, RGBColour.BLUE, 5); simply draws a blue dot where the corner is
+
+                    frame.drawShapeFilled(new Ellipse(cornerPos.x, cornerPos.y, 6, 6, 0), RGBColour.WHITE);
+                    frame.drawShapeFilled(new Ellipse(cornerPos.x - 20, cornerPos.y - 20, 10, 10, 0), RGBColour.WHITE);
+                    frame.drawShapeFilled(new Ellipse(cornerPos.x - 50, cornerPos.y - 50, 20, 20, 0), RGBColour.WHITE);
+                    frame.drawShapeFilled(new Ellipse(cornerPos.x - 150, cornerPos.y - 150, 100, 120, 0), RGBColour.WHITE);
+                    frame.drawText("OpenIMAJ is", (int) cornerPos.x - 220, (int) cornerPos.y - 140, HersheyFont.ASTROLOGY, 20, RGBColour.BLACK);
+                    frame.drawText("Awesome", (int) cornerPos.x - 210, (int) cornerPos.y - 110, HersheyFont.ASTROLOGY, 20, RGBColour.BLACK);
+                    //frame.drawShapeFilled(new Ellipse(cornerPos.x - 10, cornerPos.y - 10, 10, 10, 0), RGBColour.WHITE);
+
+                    /*
+                    frame.drawShapeFilled(new Ellipse(650f, 425f, 25f, 12f, 0f), RGBColour.WHITE);
+                    frame.drawShapeFilled(new Ellipse(600f, 380f, 30f, 15f, 0f), RGBColour.WHITE);
+                    frame.drawShapeFilled(new Ellipse(500f, 300f, 100f, 70f, 0f), RGBColour.WHITE);
+                    frame.drawText("OpenIMAJ is", 425, 300, HersheyFont.ASTROLOGY, 20, RGBColour.BLACK);
+                    frame.drawText("Awesome", 425, 330, HersheyFont.ASTROLOGY, 20, RGBColour.BLACK);
+                    */
+                    //DisplayUtilities.display(frame);
+
+
 
                 }
 
