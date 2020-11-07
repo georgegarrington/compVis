0df32d576b4e9e4bf621af4a2e92a5bcb0206327
georgegarrington <47332406+georgegarrington@users.noreply.github.com>
2020-11-07 20:10:02 +0000

    Added rotation of the kernel to make it actually a convolution and not just a correlation

--- coursework2/src/main/java/uk/ac/soton/ecs/gg2g17/hybridimages/MyConvolution.java
+++ coursework2/src/main/java/uk/ac/soton/ecs/gg2g17/hybridimages/MyConvolution.java
@@ -1,14 +1,13 @@
 package uk.ac.soton.ecs.gg2g17.hybridimages;
 
 import org.openimaj.image.FImage;
-import org.openimaj.image.processing.convolution.Gaussian2D;
 import org.openimaj.image.processor.SinglebandImageProcessor;
-
-import java.util.Arrays;
 
 public class MyConvolution implements SinglebandImageProcessor<Float, FImage> {
 
     private static float[][] kernel;
+
+    //The width and height of the kernel
     private static int kWidth;
     private static int kHeight;
 
@@ -17,24 +16,42 @@ public class MyConvolution implements SinglebandImageProcessor<Float, FImage> {
      * @param kernel
      * @throws Exception
      */
-    public MyConvolution(float[][] kernel) throws Exception{
+    public MyConvolution(float[][] kernel) {
+
+        if(kernel.length % 2 == 0 || kernel[0].length % 2 == 0){
+            System.err.println("Both dimensions of the kernel must be odd!");
+            return;
+        }
 
         for(int i = 0; i < kernel.length - 1; i++){
 
-            if(kernel[i].length != kernel[i + 1].length)
-                throw new Exception("The kernel is of an inconsistent dimension this is not allowed!");
+            if(kernel[i].length != kernel[i + 1].length){
+                System.err.println("The kernel is of an inconsistent dimension this is not allowed!");
+                return;
+            }
 
         }
-
-        if(kernel.length % 2 == 0 || kernel[0].length % 2 == 0){
-            throw new Exception("Both dimensions of the kernel must be odd!");
-        }
-
-        this.kernel = kernel;
 
         //The dimensionality has been verified so the "width" of the kernel is simply the length of the first inner array
         kWidth = kernel[0].length;
         kHeight = kernel.length;
+
+        /*
+        flip the kernel horizontally and vertically, which is the same as "rotating" 180 degrees
+        in order to make the process a true convolution and not a correlation
+         */
+        float[][] rotated = new float[kernel.length][kernel[0].length];
+        for(int i = 0; i < kernel.length; i++){
+
+            for(int j = 0; j < kernel[0].length; j++){
+
+                rotated[i][kernel[0].length - 1 - j] = kernel[kernel.length - 1 - i][j];
+
+            }
+
+        }
+
+        this.kernel = rotated;
 
     }
 
@@ -48,50 +65,57 @@ public class MyConvolution implements SinglebandImageProcessor<Float, FImage> {
 
         float[][] pixels = image.pixels;
 
+        int width = pixels[0].length;
+        int height = pixels.length;
+
         //Make the output image the same size as the image in the argument
-        float[][] convoluted = new float[pixels.length][pixels[0].length];
+        float[][] convoluted = new float[height][width];
 
         //For each row
-        for(int i = 0; i < pixels.length; i++){
+        for(int i = 0; i < height; i++){
 
-            //For each cell in the row replace it with the convoluted of the kernel
-            for(int j = 0; j < pixels[0].length; j++){
+            //For each cell in the row replace it with the convolution output of the kernel
+            for(int j = 0; j < width; j++){
 
                 /*
-                The convoluted of the kernel where the product of each cell in the kernel with their respective
+                The convolution of the kernel where the product of each cell in the kernel with their respective
                 "overlayed" pixels in the image will be added to. As zero padding is required, those cells in the
-                kernel that are outside the bounds of the image will simply be ignored and instead just add
-                all possible products possible from the valid section of the kernel that is within the image
+                kernel that are outside the bounds of the image will simply be ignored and instead just the
+                method will only add  all possible products possible from the valid section of the kernel
+                that is within the image
                  */
                 float sum = 0;
 
                 /*
-                imaginaryI and imaginaryJ keep track of the "imaginary" cell location in the kernel for instance
+                imaginaryI and imaginaryJ keep track of the "imaginary" relative cell location in the kernel for instance
                 if you were looking at pixel 0,0 and your kernel was 3x3 then imaginaryI and imaginaryJ would
                 initially be -1,-1 respectively, however as this is not valid the loop will know to skip them,
                 so you will never have an index out of bounds problem.
 
                 It will also know to move onto looking at the next iteration once imaginaryI or imaginaryJ
                 has become greater than the bounds of the image as clearly there are no longer any possible cells
-                in the kernel that are currently overlayed onto the image.
+                in the kernel that are currently overlaid onto the image.
 
                 kernelI and kernelJ keep track of the relative location of the cell being looked at in the kernel
-                that will be multiplied with the "overlay" position of this kernel cell with the pixel in the image
-                that it is "overlaid" on
+                that will be multiplied the pixel in the image that it is "overlaid" on
                 */
                 for(int imaginaryI = i - ((kHeight - 1) / 2), kernelI = 0; kernelI < kHeight; imaginaryI++, kernelI++){
 
-                    if(imaginaryI >= pixels.length)
+                    //Ignore the parts of the template positioned past the bottom of the image
+                    if(imaginaryI >= height)
                         break;
 
+                    //Ignore the parts of the template positioned above the top of the image
                     if(imaginaryI < 0)
                         continue;
 
                     for(int imaginaryJ = j - ((kWidth - 1) / 2), kernelJ = 0; kernelJ < kWidth; imaginaryJ++, kernelJ++){
 
-                        if(imaginaryJ >= pixels[0].length)
+                        //Ignore the parts of the template positioned past the right of the image
+                        if(imaginaryJ >= width)
                             break;
 
+                        //Ignore the parts of the template positioned before the left of the image
                         if(imaginaryJ < 0)
                             continue;
 
--- coursework2/src/main/java/uk/ac/soton/ecs/gg2g17/hybridimages/MyHybridImages.java
+++ coursework2/src/main/java/uk/ac/soton/ecs/gg2g17/hybridimages/MyHybridImages.java
@@ -4,128 +4,17 @@ import org.apache.commons.vfs2.FileSystemException;
 import org.openimaj.data.dataset.VFSGroupDataset;
 import org.openimaj.data.dataset.VFSListDataset;
 import org.openimaj.image.DisplayUtilities;
-import org.openimaj.image.FImage;
 import org.openimaj.image.ImageUtilities;
 import org.openimaj.image.MBFImage;
 import org.openimaj.image.processing.convolution.Gaussian2D;
 
 import java.io.File;
-import java.util.Arrays;
 import java.util.Map;
 
 public class MyHybridImages {
 
-    //JUST FOR TESTING DELETE LATER
     public static void main(String[] args){
-
-        /*
-        This is where I have stored my images, change the path to where
-        the image pairs you want to test are. Ensure that the pairs are
-        stored together in a folder containing only those 2 pairs for
-        instance perhaps the cat and dog images are stored together in a folder
-        called "catDog"
-         */
-        VFSGroupDataset<MBFImage> images = importImages(new String[]{
-
-                //Use this to get the actual value of "~" as relative paths are forbidden
-                System.getProperty("user.home"), "Downloads", "data",
-
-        });
-
-        /*
-        for(MBFImage image : images.get("catDog")){
-            DisplayUtilities.display(image);
-        }*/
-
-        /*
-        DisplayUtilities.display(cat);
-        DisplayUtilities.display(dog);*/
-
-
-        for(Map.Entry<String, VFSListDataset<MBFImage>> entry : images.entrySet()){
-
-            //Only look at the cat and dog for now
-            if(!entry.getKey().equals("borisTrump"))
-                continue;
-
-            VFSListDataset<MBFImage> pair = entry.getValue();
-            MBFImage cat = pair.get(0);
-            MBFImage dog = pair.get(1);
-            //DisplayUtilities.display(cat, "Cat");
-            //DisplayUtilities.display(dog, "Dog");
-
-            DisplayUtilities.display(makeHybrid(dog, 4, cat, 9));
-
-            /*
-            int sigma = 4;
-            int size = calcKernelSize(sigma);
-            float[][] kernel = Gaussian2D.createKernelImage(size, sigma).pixels;
-
-            try {
-                MBFImage lowPassedDog = dog.process(new MyConvolution(kernel));
-                DisplayUtilities.display(lowPassedDog);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            sigma = 9;
-            size = calcKernelSize(sigma);
-            kernel = Gaussian2D.createKernelImage(size, sigma).pixels;
-
-            try {
-                MBFImage highPassedCat = cat.subtract(cat.process(new MyConvolution(kernel)));
-                DisplayUtilities.display(highPassedCat.add(0.5f));
-            } catch (Exception e) {
-                e.printStackTrace();
-            }*/
-
-        }
-
-    }
-
-    /**
-     * Given the path of images, import them into a VFSListDataset
-     * @param path
-     * @return
-     */
-    public static VFSGroupDataset<MBFImage> importImages(String[] path){
-
-        VFSGroupDataset<MBFImage> images = null;
-
-        try {
-            images = new VFSGroupDataset<MBFImage>(generatePath(path), ImageUtilities.MBFIMAGE_READER);
-        } catch (FileSystemException e) {
-            e.printStackTrace();
-        }
-
-        /*
-        for(final Map.Entry<String, VFSListDataset<MBFImage>> entry : images.entrySet()){
-
-            DisplayUtilities.display(entry.getKey(), entry.getValue());
-
-        }*/
-
-        return images;
-
-    }
-
-    /**
-     * Generate the platform specific file path
-     * @param path
-     * @return
-     */
-    public static String generatePath(String[] path){
-
-        String out = path[0];
-
-        for(int i = 1; i < path.length; i++){
-
-            out += File.separator + path[i];
-
-        }
-
-        return out;
-
+        testWithDefault();
     }
 
     /**
@@ -146,52 +35,13 @@ public class MyHybridImages {
 
         int lowKernelSize = calcKernelSize(lowSigma);
         float[][] lowKernel = Gaussian2D.createKernelImage(lowKernelSize ,lowSigma).pixels;
-
-        MBFImage lowPassed = null;
-
-        try {
-            lowPassed = lowImage.process(new MyConvolution(lowKernel));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        MBFImage lowPassed = lowImage.process(new MyConvolution(lowKernel));
 
         int highKernelSize = calcKernelSize(highSigma);
         float[][] highKernel = Gaussian2D.createKernelImage(highKernelSize, highSigma).pixels;
-
-        MBFImage highPassed = null;
-
-        try {
-            highPassed = highImage.subtract(highImage.process(new MyConvolution(highKernel)));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        MBFImage highPassed = highImage.subtract(highImage.process(new MyConvolution(highKernel)));
 
         return highPassed.add(lowPassed);
-
-
-        /*
-        int sigma = 4;
-            int size = calcKernelSize(sigma);
-            float[][] kernel = Gaussian2D.createKernelImage(size, sigma).pixels;
-
-            try {
-                MBFImage lowPassedDog = dog.process(new MyConvolution(kernel));
-                DisplayUtilities.display(lowPassedDog);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            sigma = 9;
-            size = calcKernelSize(sigma);
-            kernel = Gaussian2D.createKernelImage(size, sigma).pixels;
-
-            try {
-                MBFImage highPassedCat = cat.subtract(cat.process(new MyConvolution(kernel)));
-                DisplayUtilities.display(highPassedCat.add(0.5f));
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-         */
 
     }
 
@@ -206,37 +56,84 @@ public class MyHybridImages {
         return size;
     }
 
-    public static MBFImage lowPassFilter(MBFImage image, float sigma){
+    /**
+     * The code I used to generate my scale visualisation of the hybrid image of trump and an oompa loompa
+     */
+    public static void generateTrumpOompaLoompaHybridImage(){
 
-        int size = calcKernelSize(sigma);
-        float[][] kernel = Gaussian2D.createKernelImage(size, sigma).pixels;
 
-        //Exception handling to ensure valid dimensionality and identical "row" sizes in the image
+
+    }
+
+    /**
+     * Given the path of image pairs contained in individual directories, import them into a VFSGroupDataset
+     * @param arr the path of the directory in array form
+     * @return
+     */
+    public static VFSGroupDataset<MBFImage> importImages(String[] arr){
+
+        VFSGroupDataset<MBFImage> images = null;
+
+        String path = arr[0];
+
+        for(int i = 1; i < arr.length; i++){
+
+            path += File.separator + arr[i];
+
+        }
+
         try {
+            images = new VFSGroupDataset<MBFImage>(path, ImageUtilities.MBFIMAGE_READER);
+        } catch (FileSystemException e) {
+            e.printStackTrace();
+        }
 
-            /*
-            process will clone the instance it is being called on, then process
-            it with the given Processor and return the result in a new image instance
-             */
-            image.process(new MyConvolution(kernel));
+        return images;
+
+    }
+
+    /**
+     * An example of using the test method with a directory on my device
+     */
+    public static void testWithDefault(){
+
+        try {
+            test(new String[]{System.getProperty("user.home"), "Downloads", "data"});
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        return image;
-
     }
 
-    public static MBFImage highPassFilter(MBFImage original, float sigma){
-
-        MBFImage lowPassed = lowPassFilter(original, sigma);
+    /**
+     * Test the images with some pairs from the path given in the form of an array of strings
+     * so that it works on all platforms
+     */
+    public static void test(String[] arr) throws Exception {
 
         /*
-        subtract will clone the instance it is being called on, subtract the
-        argument and then return in a new image instance
+        This is where I have stored my images, change the path to where
+        the image pairs you want to test are. Ensure that the pairs are
+        stored together in a folder containing only those 2 pairs for
+        instance perhaps the cat and dog images are stored together in a folder
+        called "catDog"
          */
-        MBFImage rtn = original.clone();
-        return rtn.subtract(lowPassed);
+        VFSGroupDataset<MBFImage> images = importImages(arr);
+
+        for(Map.Entry<String, VFSListDataset<MBFImage>> entry : images.entrySet()){
+
+            VFSListDataset<MBFImage> pair = entry.getValue();
+
+            if(pair.size() != 2)
+                throw new Exception("Make sure that you have only given pairs of images contained in" +
+                        "\n individual directories within the directory for images");
+
+            MBFImage fst = pair.get(0);
+            MBFImage snd = pair.get(1);
+
+            DisplayUtilities.display(makeHybrid(fst, 5, snd, 9), entry.getKey());
+
+        }
 
     }
 
