83d1f53fc52745f8a3231630fcfacae08c24a06a
georgegarrington <47332406+georgegarrington@users.noreply.github.com>
2020-11-08 12:58:51 +0000

    Completely ready for handin now

--- coursework2/src/main/java/uk/ac/soton/ecs/gg2g17/hybridimages/MyHybridImages.java
+++ coursework2/src/main/java/uk/ac/soton/ecs/gg2g17/hybridimages/MyHybridImages.java
@@ -10,13 +10,10 @@ import org.openimaj.image.processing.convolution.Gaussian2D;
 import org.openimaj.image.processing.resize.ResizeProcessor;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 
 public class MyHybridImages {
-
-    public static void main(String[] args){
-        testWithDefault();
-    }
 
     /**
      * Compute a hybrid image combining low-pass and high-pass filtered images
@@ -58,15 +55,10 @@ public class MyHybridImages {
     }
 
     /**
-     * The code I used to generate my scale visualisation of the hybrid image of trump and an oompa loompa
+     * Display a downsampling visualization of an image as shown in the spec
+     * @param image
      */
-    public static void generateTrumpOompaLoompaHybridImage(){
-
-
-
-    }
-
-    public static void genDownSample(MBFImage image){
+    public static void visualizeDownSample(MBFImage image){
 
         /*
         The images that will be displayed, show 5 different sizes where each successive
@@ -88,16 +80,13 @@ public class MyHybridImages {
         for(MBFImage i : images){
 
             diagramWidth += i.getWidth();
-            System.out.println("The dimensions of the image are: " + i.getWidth() + ", " + i.getHeight());
 
         }
 
         MBFImage diagram = new MBFImage(diagramWidth, diagramHeight);
 
-        System.out.println("The dimensions of the diagram are: " + diagramWidth + ", " + diagramHeight);
-
+        //Make the background white
         diagram.addInplace(255f);
-
         diagram.drawImage(images[0], 0, 0);
 
         //x and y coordinates to plot the image, i to count the index of the array
@@ -114,9 +103,24 @@ public class MyHybridImages {
     }
 
     /**
+     * Save the given image as a jpeg with the given file name
+     * @param image
+     * @param fileName
+     */
+    public static void export(MBFImage image, String fileName){
+
+        try {
+            ImageUtilities.write(image, new File(fileName + ".jpeg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
      * Given the path of image pairs contained in individual directories, import them into a VFSGroupDataset
      * @param arr the path of the directory in array form
-     * @return
+     * @return images grouped in pairs by the directory they are both contained in
      */
     public static VFSGroupDataset<MBFImage> importImages(String[] arr){
 
@@ -141,23 +145,22 @@ public class MyHybridImages {
     }
 
     /**
-     * An example of using the test method with a directory on my device
+     * An example of using the test method with a directory on my device, you can
+     * simply change the string array in the definition to the string array of the
+     * directory containing the pairs you want to test (and obviously tweak sigma
+     * vals and image order etc in the test method)
      */
     public static void testWithDefault(){
 
-        try {
-            test(new String[]{System.getProperty("user.home"), "Downloads", "data"});
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        test(new String[]{System.getProperty("user.home"), "Downloads", "data"});
 
     }
 
     /**
-     * Test the images with some pairs from the path given in the form of an array of strings
-     * so that it works on all platforms
+     * Test the images with the pairs contained in the path given in the
+     * form of an array of strings so that it works on all platforms
      */
-    public static void test(String[] arr) throws Exception {
+    public static void test(String[] arr) {
 
         /*
         This is where I have stored my images, change the path to where
@@ -170,25 +173,21 @@ public class MyHybridImages {
 
         for(Map.Entry<String, VFSListDataset<MBFImage>> entry : images.entrySet()){
 
-            //FOR TESTING DELETE AFTER
-            if(!entry.getKey().equals("trumpLumpa"))
-                continue;
-
             VFSListDataset<MBFImage> pair = entry.getValue();
 
-            if(pair.size() != 2)
-                throw new Exception("Make sure that you have only given pairs of images contained in" +
+            if(pair.size() != 2) System.err.println("Make sure that you have only given pairs of images contained in" +
                         "\n individual directories within the directory for images!");
 
             MBFImage fst = pair.get(0);
             MBFImage snd = pair.get(1);
 
-            MBFImage hybrid = makeHybrid(fst, 5, snd, 9);
-
-            genDownSample(hybrid);
-
-            //THIS IS HOW TO MAKE THE TRUMP / OOMPA LOOMPA IMAGE DONT CHANGE IT
-            //DisplayUtilities.display(makeHybrid(fst, 5, snd, 9), entry.getKey());
+            /*
+            This is the best parameters and orientation for generating the trump/oompa loompa hybrid
+            It is different for each two images so change this for each e.g. sigma vals and swapping
+            image arguments
+             */
+            MBFImage hybrid = makeHybrid(fst, 3, snd, 10);
+            visualizeDownSample(hybrid);
 
         }
 
